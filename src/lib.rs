af1909306b80416a18e12253859bb3379efd71bb
王继荣 <ximengwuheng@163.com>
2026-06-05 07:00:26 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
-pub struct Source {}
+use std::{pin::Pin, sync::Arc};
+use tokio::sync::broadcast;
 
-pub struct Machine {}
-
-pub enum Event {
-    Noti,
+pub struct Source<T> {
+    pub value: Arc<T>,
+    sender: Arc<broadcast::Sender<T>>,
 }
 
-pub trait AsStateMachine {}
+impl<T> Source<T> {
+    pub fn reader(&self) -> Reader<T> {
+        Reader {
+            value: self.value.clone(),
+            sender: self.sender.clone(),
+        }
+    }
+}
 
-pub trait AsSubscriber<T> {}
+pub struct Reader<T> {
+    pub value: Arc<T>,
+    sender: Arc<broadcast::Sender<T>>,
+}
+
+pub trait AsStateMachine {
+    type ChangeEvent;
+}
+
+pub trait AsSubscriber<S, T> {
+    fn subscribe(
+        &self,
+        reader: &Reader<T>,
+        convert: impl Fn(S) -> Pin<Box<dyn Future<Output = T>>>,
+    ) {
+        //
+    }
+}
 
 #[cfg(test)]
 mod tests {
