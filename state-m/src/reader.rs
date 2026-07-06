e3442561f1e1e6de0b5887511ae945feac5f5e7e
wangjirong <wangjirong@dreame.tech>
2026-07-06 10:44:24 +0800

    update

--- state-m/src/reader.rs
+++ state-m/src/reader.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use crossfire::mpmc::{self, List};
 use futures::StreamExt;
-use std::fmt::Debug;
+use std::{fmt::Debug, pin::Pin, sync::Arc};
 
 pub struct Reader<S>
 where
@@ -32,6 +32,39 @@ where
                 is_touch: s.is_touch,
                 close_handle: s.close_handle,
             })
+        });
+        tokio::spawn(stream.forward(sink));
+        Reader {
+            stream: AsyncStreamWrapper(rx.into_stream()),
+        }
+    }
+}
+
+impl<S> Reader<S>
+where
+    S: 'static + Clone + Debug + Default + PartialEq + Send,
+{
+    pub fn extend_with<T, F, Fut>(self, f: F) -> Reader<T>
+    where
+        T: 'static + Clone + Debug + Default + PartialEq + Send,
+        F: Fn(S) -> Fut + Send + Sync + 'static,
+        Fut: Future<Output = T> + Send,
+    {
+        let (tx, rx) = mpmc::unbounded_async();
+        let sink = AsyncSinkWrapper(tx.into_async().into_sink());
+        let arc_f = Arc::new(f);
+        let stream = self.stream.then(move |s| {
+            let arc_fc = arc_f.clone();
+            async move {
+                Ok(StateEvent {
+                    state: State {
+                        value: arc_fc.as_ref()(s.state.value).await,
+                        timestamp: s.state.timestamp,
+                    },
+                    is_touch: s.is_touch,
+                    close_handle: s.close_handle,
+                })
+            }
         });
         tokio::spawn(stream.forward(sink));
         Reader {
