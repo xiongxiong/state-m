a8bde3447383d834fb7fc1dac88a9090447d69e8
wangjirong <wangjirong@dreame.tech>
2026-07-10 14:12:36 +0800

    update

--- state-m-macro/src/lib.rs
+++ state-m-macro/src/lib.rs
@@ -18,6 +18,24 @@ impl Parse for KvAssocArgs {
     }
 }
 
+fn parse_kv_assoc_args(attrs: Vec<Attribute>) -> KvAssocArgs {
+    let kv_assoc_attrs: Vec<&Attribute> = attrs
+        .iter()
+        .filter(|v| v.path().is_ident("kv_assoc"))
+        .collect();
+    match kv_assoc_attrs.len() {
+        0 => panic!("[kv_assoc] attribute not found."),
+        1 => {
+            let attr = kv_assoc_attrs.get(0).unwrap();
+            let args: KvAssocArgs = attr
+                .parse_args()
+                .unwrap_or_else(|e| panic!("[kv_assoc] attr invalid : {}", e));
+            args
+        }
+        _ => panic!("[kv_assoc] attribute should not appear more than once."),
+    }
+}
+
 #[proc_macro_attribute]
 pub fn state_tag(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
@@ -27,24 +45,32 @@ pub fn state_tag(_attr: TokenStream, item: TokenStream) -> TokenStream {
     match input.data {
         syn::Data::Enum(data_enum) => {
             for item in data_enum.variants {
-                let ident = &item.ident;
-                let fields = &item.fields;
+                let ident = item.ident;
+                let fields = item.fields;
                 let q_name = format_ident!("{}{}", k_name, ident);
                 let q = quote! {
                     #k_vis struct #q_name #fields
                 };
                 quotes.push(q);
 
-                for attr in &item.attrs {
-                    if attr.path().is_ident("kv_assoc") {
-                        let args: KvAssocArgs = attr
-                            .parse_args()
-                            .unwrap_or_else(|e| panic!("[kv_assoc] attr invalid : {}", e));
+                let kv_value_typ = parse_kv_assoc_args(item.attrs).0;
+                let q_kv_assoc = quote! {
+                    impl KvAssoc for #q_name {
+                        type Value = #kv_value_typ;
                     }
-                }
+                };
+                quotes.push(q_kv_assoc);
             }
         }
-        _ => panic!("[state_tag] attribute macro can only be used on enum data type."),
+        syn::Data::Struct(_) | syn::Data::Union(_) => {
+            let kv_value_typ = parse_kv_assoc_args(input.attrs).0;
+            let q = quote! {
+                impl KvAssoc for #k_name {
+                    type Value = #kv_value_typ;
+                }
+            };
+            quotes.push(q);
+        }
     };
     quote! {
         #(#quotes)*
