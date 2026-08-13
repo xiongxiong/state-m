c0f67c7ab5898b22cc36128cfb40fd523e86c396
wangjirong <wangjirong@dreame.tech>
2026-08-13 13:42:05 +0800

    fix: update

--- state-m/tests/expand.rs
+++ state-m/tests/expand.rs
@@ -1,6 +1,6 @@
-use state_m::{AsKey, KvAssoc};
-use state_m_macro::*;
-use std::{fmt::Debug, hash::Hash};
+// use state_m::{AsKey, KvAssoc};
+// use state_m_macro::*;
+// use std::{fmt::Debug, hash::Hash};
 
 // sm_watch!(1);
 
